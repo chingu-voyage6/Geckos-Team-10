75741de2a1005aa5b32270253aa47644e1daa1f7
Nicholas  Vorraso <nvorraso2020@gmail.com>
2018-06-14 15:01:02 -0400

    :sparkles: (all changes are contained to the components directory) Installed react-router-dom and a…

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import Home from '../Home/Home.js';
+import Login from '../Login/Login.js';
+import Signup from '../Signup/Signup.js';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-routes">
+        <Router>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
       </div>
     );
   }
