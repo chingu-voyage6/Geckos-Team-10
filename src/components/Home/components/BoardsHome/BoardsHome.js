49f4d7e58465a041dc6ae241eed0d7194fb0c36a
Nicholas  Vorraso <nvorraso2020@gmail.com>
2018-07-16 23:35:13 -0400

    :recycle: (BoardsHome.js) Retrieves all data in one request instead of two.

--- src/components/Home/components/BoardsHome/BoardsHome.js
+++ src/components/Home/components/BoardsHome/BoardsHome.js
@@ -5,9 +5,20 @@ import gql from 'graphql-tag'
 import { Card, Wrapper, CardTitle } from './BoardsHome.styles'
 import { Title, Icon } from '../../../StyledComponents/index'
 
-const TeamBoardsQuery = gql`
+
+const BoardsQuery = gql`
   query user($id: ID){
     User (id: $id) {
+      id
+      boards {
+        id
+        title
+        background
+        team {
+          id
+          name
+        }
+      }
       teams {
         id
         name
@@ -21,68 +32,44 @@ const TeamBoardsQuery = gql`
   }
 `
 
-const PersonalBoardsQuery = gql`
-  query user($id: ID){
-    User (id: $id) {
-      id
-      boards {
-        id
-        title
-        background
-        team {
-          id
-          name
-        }
-      }
-    }
-  }
-`
-
 const BoardsHome = props => {
   return (
     <div>
-      {console.log(props.userId)}
-      <Query query={PersonalBoardsQuery} variables={{ id: props.userId }}>
+      <Query query={BoardsQuery} variables={{ id: props.userId }}>
         {({ loading, data }) => {
           return (
-            <Fragment>
-              <Title><Icon grey medium className="fa fa-user" /> Personal</Title>
-              <Wrapper>
-                {
-                  !loading && data && data.User.boards.map(board =>
-                    (board.team == null &&
-                      <Card key={board.id} style={{ background: board.background }}>
-                        <CardTitle>{board.title}</CardTitle>
-                      </Card>
-                    )
-                  )
-                }
-                <Card>Create new board..</Card>
-              </Wrapper>
-            </Fragment>
-          )
-        }}
-      </Query>
-      <Query query={TeamBoardsQuery} variables={{ id: props.userId }}>
-        {({ loading, data }) => {
-          return (
-            <Fragment>
-              {
-                !loading && data && data.User.teams.map(team => (
-                  <Fragment>
-                    <Title><Icon grey medium className="fa fa-users" />{team.name}</Title>
-                    <Wrapper>
-                      {team.boards.map(board => (
+            !loading && data && (
+              <Fragment>
+                <Title><Icon grey medium className="fa fa-user" /> Personal</Title>
+                <Wrapper>
+                  {
+                    data.User.boards.map(board =>
+                      (board.team == null &&
                         <Card key={board.id} style={{ background: board.background }}>
                           <CardTitle>{board.title}</CardTitle>
-                        </Card>))
-                      }
-                      <Card>Create new board..</Card>
-                    </Wrapper>
-                  </Fragment>)
-                )
-              }
-            </Fragment>
+                        </Card>
+                      )
+                    )
+                  }
+                  <Card>Create new board..</Card>
+                </Wrapper>
+                {
+                  data.User.teams.map(team => (
+                    <Fragment>
+                      <Title><Icon grey medium className="fa fa-users" />{team.name}</Title>
+                      <Wrapper>
+                        {team.boards.map(board => (
+                          <Card key={board.id} style={{ background: board.background }}>
+                            <CardTitle>{board.title}</CardTitle>
+                          </Card>))
+                        }
+                        <Card>Create new board..</Card>
+                      </Wrapper>
+                    </Fragment>)
+                  )
+                }
+              </Fragment>
+            )
           )
         }}
       </Query>
