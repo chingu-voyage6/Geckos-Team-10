b734554436e0c1491cea7ddfb83bb1fe4d66756d
Alberto Escala <albertoaesc@gmail.com>
2020-10-03 23:55:09 -0500

    change createTeam component form class to hook

--- src/components/CreateTeam/CreateTeam.js
+++ src/components/CreateTeam/CreateTeam.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { withApollo } from 'react-apollo'
 
 import { Button, Heading, Icon, Input, TextArea, Wrapper } from '../StyledComponents'
 
-class CreateTeam extends Component {
-  state = {
-    name: '', desc: ''
-  }
+const CreateTeam = props => {
+  const [team, setTeam] = useState({
+    name: '',
+    desc: ''
+  })
 
-  onChange = e => {
+  const onChange = e => {
     if (e.target.id === 'name') {
-      this.setState({ name: e.target.value })
+      setTeam({ name: e.target.value })
     } else {
-      this.setState({ desc: e.target.value })
+      setTeam({ desc: e.target.value })
     }
   }
 
-  handleSubmit = () => {
-    const { userId, createTeam } = this.props
-    const { name, desc } = this.state
+  const handleSubmit = () => {
+    const { userId, createTeam } = props
+    const { name, desc } = team
 
     createTeam(userId, name, desc)
   }
 
-  render() {
-    const { hasChevron, goBack } = this.props
-    return (
-      <Wrapper width="100%">
-        <Heading>
-          {hasChevron ?
-            <Wrapper flex align>
-              <Button
-                width="10%"
-                marginLeft="0"
-                hoverBackgroundColor="white"
-                onClick={goBack}
-              ><Icon grey className="fa fa-chevron-left" />
-              </Button>
-              <div style={{ textAlign: 'center', width: '80%' }}>Create Team</div>
-            </Wrapper> :
-            <span>Create Team</span>
-          }
-        </Heading>
-        <br />
-        <span>Name</span>
-        <Input wide id="name" onChange={this.onChange} />
-        <br />
-        <span>Description (optional)</span>
-        <TextArea wide id="desc" onChange={this.onChange} />
-        <br />
-        <Button solid onClick={this.handleSubmit}>Save</Button>
-      </Wrapper>
-    )
-  }
+  const { hasChevron, goBack } = props
+
+  return (
+    <Wrapper width="100%">
+      <Heading>
+        {hasChevron ?
+          <Wrapper flex align>
+            <Button
+              width="10%"
+              marginLeft="0"
+              hoverBackgroundColor="white"
+              onClick={goBack}
+            ><Icon grey className="fa fa-chevron-left" />
+            </Button>
+            <div style={{ textAlign: 'center', width: '80%' }}>Create Team</div>
+          </Wrapper> :
+          <span>Create Team</span>
+        }
+      </Heading>
+      <br />
+      <span>Name</span>
+      <Input wide id="name" onChange={onChange} />
+      <br />
+      <span>Description (optional)</span>
+      <TextArea wide id="desc" onChange={onChange} />
+      <br />
+      <Button solid onClick={handleSubmit}>Save</Button>
+    </Wrapper>
+  )
 }
 
 const CreateTeamWithApollo = withApollo(CreateTeam)
