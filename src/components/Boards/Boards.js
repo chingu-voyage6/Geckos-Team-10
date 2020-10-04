e638b258f7434fec75a0cfc18f5e37049593d252
Alberto Escala <albertoaesc@gmail.com>
2020-10-04 00:18:53 -0500

    uodate Board component

--- src/components/Boards/Boards.js
+++ src/components/Boards/Boards.js
@@ -1,62 +1,58 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 
 import { Wrapper, Button, MenuHeader, Text } from './Boards.styles'
 
 import ZippyMenu from './ZippyMenu/ZippyMenu'
 
-class Boards extends Component {
-  componentDidMount = () => {
-    document.addEventListener('mousedown', this.handleClickOutside)
-  }
-
-  componentWillUnmount = () => {
-    document.removeEventListener('mousedown', this.handleClickOutside)
-  }
-
-  setBoardsRef = node => {
-    this.boardsRef = node
-  }
-
-  handleClickOutside = event => {
+const Boards = () => {
+  const handleClickOutside = event => {
     if (this.boardsRef && event.target.id !== 'toggle_boards' && !this.boardsRef.contains(event.target)) {
       this.props.toggleBoards(false)
     }
   }
 
-  render() {
-    const { boards, teams } = this.props
-    const ContentJSX = (
-      <Fragment>
-        {this.props.keepOpen &&
-          <MenuHeader>
-            <Text black>Menu</Text>
-          </MenuHeader>
-        }
-        {/* <Input type="search" placeholder="Find boards by name..." /> */
-        /* <ZippyMenu title="Starred Boards" /> */
-        /* <ZippyMenu title="Recent Boards" /> */}
-        <ZippyMenu title="All Boards" boards={boards} />
-        {teams && teams.map(team => {
-          return <ZippyMenu key={team.id} teamId={team.id} title={team.name} boards={team.boards} />
-        })}
-        <Button secondary onClick={this.props.toggleFixedMenu}>
-          {`${(this.props.keepOpen ? 'Don\'t' : 'Always')} keep this tab open`}
-        </Button>
-      </Fragment>
-    )
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+  }, [])
 
-    return (
-      this.props.keepOpen ?
-        <Wrapper alwaysOpen>
+  useEffect(() => {
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  const { boards, teams } = this.props
+  const ContentJSX = (
+    <Fragment>
+      {this.props.keepOpen &&
+        <MenuHeader>
+          <Text black>Menu</Text>
+        </MenuHeader>
+      }
+      {/* <Input type="search" placeholder="Find boards by name..." /> */
+      /* <ZippyMenu title="Starred Boards" /> */
+      /* <ZippyMenu title="Recent Boards" /> */}
+      <ZippyMenu title="All Boards" boards={boards} />
+      {teams && teams.map(team => {
+        return <ZippyMenu key={team.id} teamId={team.id} title={team.name} boards={team.boards} />
+      })}
+      <Button secondary onClick={this.props.toggleFixedMenu}>
+        {`${(this.props.keepOpen ? 'Don\'t' : 'Always')} keep this tab open`}
+      </Button>
+    </Fragment>
+  )
+
+  return (
+    this.props.keepOpen ?
+      <Wrapper alwaysOpen>
+        {ContentJSX}
+      </Wrapper> :
+      <div ref={this.setBoardsRef}>
+        <Wrapper notAlwaysOpen>
           {ContentJSX}
-        </Wrapper> :
-        <div ref={this.setBoardsRef}>
-          <Wrapper notAlwaysOpen>
-            {ContentJSX}
-          </Wrapper>
-        </div>
-    )
-  }
+        </Wrapper>
+      </div>
+  )
 }
 
 export default Boards
